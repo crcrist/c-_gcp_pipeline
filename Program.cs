ab2733fd7fdfc55de6642b38d85fb854201ab102
Keegan <keeganpatton@gmail.com>
2024-08-03 15:21:58 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// for data frame like stuff I should look into the DataTable class in System.Data namespace.
+// importing packages is done by "using <package>"
+
+
+using System;
+using Google.Cloud.BigQuery.V2;
+
+namespace BigQueryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = BigQueryClient.Create("projectId");
+            var table = client.GetTable("bigquery-public-data", "samples", "shakespeare");
+            var sql = $"SELECT corpus AS title, COUNT(word) AS unique_words FROM {table} GROUP BY title ORDER BY unique_words DESC LIMIT 10";
+
+            var results = client.ExecuteQuery(sql, parameters: null);
+
+            foreach (var row in results)
+            {
+                Console.WriteLine($"{row["title"]}: {row["unique_words"]}");
+            }
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# .NET BigQuery Data Pipeline
+
+### A pipeline that will eventually source data from GCP, transform it a bit and place it in a local Postgres DB.
