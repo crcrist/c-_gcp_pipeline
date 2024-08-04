6622d214d8a804a4edc18d13657f0ff9e06ad41f
Keegan <keeganpatton@gmail.com>
2024-08-03 19:50:13 -0500

    update readme

--- Program.cs
+++ Program.cs
@@ -1,38 +1,38 @@
-﻿// for data frame like stuff I should look into the DataTable class in System.Data namespace.
-// importing packages is done by "using <package>"
-// added this package for DI dotnet add package Microsoft.Extensions.DependencyInjection
-
-
-using System;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace BigQueryDemo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Setup DI
-            var serviceProvider = new ServiceCollection()
-                .AddSingleton<IBigQueryService>(provider => new BigQueryService("projectId"))
-                .BuildServiceProvider();
-
-            // Get service
-            var bigQueryService = serviceProvider.GetService<IBigQueryService>();
-
-            // Define SQL query
-            var sql = "SELECT corpus AS title, COUNT(word) AS unique_words FROM `bigquery-public-data.samples.shakespeare` GROUP BY title ORDER BY unique_words DESC LIMIT 10";
-
-            // Execute query
-            var results = bigQueryService.ExecuteQuery(sql);
-
-            // Display results
-            foreach (var row in results)
-            {
-                Console.WriteLine($"{row["title"]}: {row["unique_words"]}");
-            }
-        }
-    }
-}
-
+﻿// for data frame like stuff I should look into the DataTable class in System.Data namespace.
+// importing packages is done by "using <package>"
+// added this package for DI dotnet add package Microsoft.Extensions.DependencyInjection
+
+
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BigQueryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup DI
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IBigQueryService>(provider => new BigQueryService("projectId"))
+                .BuildServiceProvider();
+
+            // Get service
+            var bigQueryService = serviceProvider.GetService<IBigQueryService>();
+
+            // Define SQL query
+            var sql = "SELECT corpus AS title, COUNT(word) AS unique_words FROM `bigquery-public-data.samples.shakespeare` GROUP BY title ORDER BY unique_words DESC LIMIT 10";
+
+            // Execute query
+            var results = bigQueryService.ExecuteQuery(sql);
+
+            // Display results
+            foreach (var row in results)
+            {
+                Console.WriteLine($"{row["title"]}: {row["unique_words"]}");
+            }
+        }
+    }
+}
+
 
--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# .NET BigQuery Data Pipeline
+# .NET BigQuery Data Pipeline 
 
-### A pipeline that will eventually source data from GCP, transform it a bit and place it in a local Postgres DB.
+ ## Overview
+ Data Pipeline using **.NET**, BigQuery, and Postgres.
+
+## Why does this exist?
+This is a practice in **co-programming** and modern .NET ETL workflows. Our main objective is to utilize each other's strengths and gain a better understanding of the relationship between GCP and .NET.
+
+## Project Tooling
+| Cloud Storage | Local Storage | Development |
+|---------------|---------------|-------------|
+|   BigQuery    |   Postgres    |    .NET     |
+
+
+## Files
+- **Program.cs** - *Main application file.*
+- **BigQueryService.cs** - BQ API Query logic. This is part of a proposal made by  [ccrist](https://github.com/crcrist) based on his experience with C#.
+- **c_sharp.csproj** - project dependencies.
+
+## ToDo 
+1. Establish mutually accessible service account in **GCP**.
+2. Create API call logic.
+3. Develop transformation logic.
+4. Create Postgres instance for landing.
+
+## Relevant Documentation
+- [gcloud CLI](https://cloud.google.com/sdk/docs/install)
+- [C# BigQuery client Library](https://cloud.google.com/bigquery/docs/reference/libraries)
+- [GCP Service Accounts](https://cloud.google.com/iam/docs/service-account-overview)
+- [Google Cloud Free Tier Docs](https://cloud.google.com/free/docs/free-cloud-features)
+- [GCP Billing Dashboard](https://console.cloud.google.com/billing)
+- [DataTable class for Tabular Data](https://learn.microsoft.com/en-us/dotnet/api/system.data.datatable?view=net-8.0)
+
+
